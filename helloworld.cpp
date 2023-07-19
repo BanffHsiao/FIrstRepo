695c7f7fcd8da18ea7f71e9a29a8f19144fa89b7
banffhsiao <139524569+BanffHsiao@users.noreply.github.com>
2023-07-19 10:57:43 +0800

    new adder function in helloworld.cpp file

--- helloworld.cpp
+++ helloworld.cpp
@@ -3,5 +3,11 @@
 int main()
 {
     std::cout << "Hello, world!" << std::endl;
+    int sum = 0;
+    for(int i = 0; i < 10; i++)
+    {
+        sum += i;
+    }
+    std::cout << "sum = " << sum << std::endl;
     return 0;
 }
